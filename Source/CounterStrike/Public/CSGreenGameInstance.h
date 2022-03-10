f048a9ff93e10bd2f5e1a54b77a55054027aa6c5
John <ufa-john@yandex.ru>
2022-03-10 20:22:58 +0500

    123

---
+++ Source/CounterStrike/Public/CSGreenGameInstance.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "CSGreenGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COUNTERSTRIKE_API UCSGreenGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+public: UCSGreenGameInstance();
+};
