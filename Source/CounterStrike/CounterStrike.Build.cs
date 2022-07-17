77c21e7f8b9d578fd21084abe7d863fc414ddd96
John <ufa-john@yandex.ru>
2022-07-17 13:53:38 +0500

    123

--- Source/CounterStrike/CounterStrike.Build.cs
+++ Source/CounterStrike/CounterStrike.Build.cs
@@ -8,7 +8,7 @@ public class CounterStrike : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OnlineSubsystem", "OnlineSubsystemEOS",  "Http", "Json", "JsonUtilities"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OnlineSubsystem", "OnlineSubsystemEOS",  "Http", "Json", "JsonUtilities", "AndroidPlayBilling"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
