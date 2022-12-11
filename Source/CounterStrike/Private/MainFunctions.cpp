5897ec560d358ecfb81647fc8691adf0c6313fba
John <ufa-john@yandex.ru>
2022-12-11 19:08:10 +0500

    123

--- Source/CounterStrike/Private/MainFunctions.cpp
+++ Source/CounterStrike/Private/MainFunctions.cpp
@@ -4,11 +4,25 @@
 #include "MainFunctions.h"
 #include "GenericPlatform/GenericPlatformProcess.h"
 
-void UMainFunctions::LaunchServer(const FString& MapAddress, const FString& Attributes)
+FWorkerProc UMainFunctions::LaunchServer(const FString& MapAddress, const FString& Attributes)
 {
 	FString ResultAttributes = MapAddress + " " + Attributes + " -log";
 	FString Url = FPaths::ProjectDir() + "Binaries/Win64/CounterStrikeServer.exe";
-	
-	FPlatformProcess::CreateProc(*Url, *ResultAttributes, true, false, false, nullptr, 0, nullptr, nullptr);
-	
+
+	FWorkerProc WorkerProc;
+	WorkerProc.WorkerProcess = FPlatformProcess::CreateProc(*Url, *ResultAttributes, true, false, false, nullptr, 0, nullptr, nullptr);
+	 
+	return WorkerProc;
 }
+
+void UMainFunctions::DeleteServer(FWorkerProc WorkProcess)
+{
+	FProcHandle WorkerProcess = WorkProcess.WorkerProcess;
+	if (WorkerProcess.IsValid())
+	{
+		FPlatformProcess::TerminateProc(WorkerProcess);
+		FPlatformProcess::CloseProc(WorkerProcess);
+	}
+}
+
+
--- Source/CounterStrike/Public/MainFunctions.h
+++ Source/CounterStrike/Public/MainFunctions.h
@@ -6,16 +6,30 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "MainFunctions.generated.h"
 
+
 /**
  * 
  */
+ USTRUCT(BlueprintType)
+struct FWorkerProc
+ {
+	GENERATED_BODY()
+ 	FProcHandle WorkerProcess;
+ };
+
+
+
+
 UCLASS()
 class COUNTERSTRIKE_API UMainFunctions : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
 
 public:
-	UFUNCTION(BlueprintCallable, Category = "Server Functions")
-	static void LaunchServer(const FString& MapAddress, const FString& Attributes);
 	
+	UFUNCTION(BlueprintCallable, Category = "Server Functions")
+	static FWorkerProc LaunchServer(const FString& MapAddress, const FString& Attributes);
+
+	UFUNCTION(BlueprintCallable, Category = "Server Functions")
+	static void DeleteServer(FWorkerProc WorkProcess);	
 };
